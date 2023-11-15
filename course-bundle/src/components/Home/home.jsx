5b7fa4e4f2d68685f7f640b0f148a3b6c87934b5
hemantmm <hemant29mehta@gmail.com>
2023-11-15 20:31:50 +0530

    first

--- course-bundle/src/components/Home/home.jsx
+++ course-bundle/src/components/Home/home.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
+import {Box, Button, HStack, Heading, Image, Stack, Text, VStack} from '@chakra-ui/react'
+import "./home.css"
+import { Link } from 'react-router-dom'
+import vg from '../../assets/images/bg.png'
+import {CgGoogle,CgYoutube} from 'react-icons/cg'
+import {DiAws} from 'react-icons/di'
+import {SiCoursera,SiUdemy} from 'react-icons/si'
+import introVideo from '../../assets/videos/intro.mp4'
+
 
 function Home() {
   return (
-    <div>Home</div>
+    <section className='home'>
+        <div className="container">
+            <Stack direction={["column","row"]} height="100%" justifyContent={["center","space-between"]} alignItems="center" spacing={['16','56']}>
+                <VStack width={"full"} alignItems={["center","flex-end"]} spacing="8">
+                    <Heading children="Learn from the experts" size={'2xl'}></Heading>
+                    <Text fontSize={'2xl'} fontFamily={'cursive'} textAlign={["center","left"]} children="Find valuable content at reasonable cost" />
+                    <Link to="/courses">
+                        <Button size={"lg"} colorScheme='yellow'>
+                            Explore
+                        </Button>
+                    </Link>
+                </VStack>
+
+                <Image className='vector-graphics' boxSize={"md"} src={vg} objectFit="contain" />
+            </Stack>
+        </div>
+
+        <Box padding={"8"} bg="blackAlpha.800">
+            <Heading textAlign={"center"} fontFamily="body" color={'yellow.400'} children="OUR BRANDS" />
+            <HStack className='brandsBanner' justifyContent={'space-evenly'} marginTop="4">
+                <CgGoogle />
+                <CgYoutube />
+                <SiCoursera />
+                <SiUdemy />
+                <DiAws />
+            </HStack>
+        </Box>
+        <div className="container2">
+            <video controls controlsList='nodownload nofullscreen noremoteplayback' disablePictureInPicture disableRemotePlayback src={introVideo}>
+
+            </video>
+        </div>
+    </section>
   )
 }
 
