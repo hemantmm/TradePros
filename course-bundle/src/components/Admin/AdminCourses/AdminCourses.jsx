1c9704380a6af8d31750ecf666e25791ce29b2f7
hemantmm <hemant29mehta@gmail.com>
2023-11-27 14:34:57 +0530

    admin course page

--- course-bundle/src/components/Admin/AdminCourses/AdminCourses.jsx
+++ course-bundle/src/components/Admin/AdminCourses/AdminCourses.jsx
@@ -1,21 +1,23 @@
-import { Box, Button, Grid, HStack, Heading, Table, TableCaption, TableContainer, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react'
+import { Box, Button, Grid, HStack, Heading, Image, Table, TableCaption, TableContainer, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react'
 import React from 'react'
 import Sidebar from '../Sidebar'
 import { RiDeleteBin7Fill } from 'react-icons/ri'
 
 function AdminCourses() {
 
-  const users=[{
+  const courses=[{
     _id:"fdasfda",
-    name:"hemant",
-    role:"admin",
-    subscription:{
-      status:'active'
+    title:"react course",
+    category:"web dev",
+    poster:{
+      url:'https://cdn.pixabay.com/photo/2017/05/10/19/29/robot-2301646_960_720.jpg'
     },
-    email:"he@gmail.com",
+    createdBy:"hemant",
+    views:299,
+    numOfVideos:12,
   }]
 
-  const updateHandler=(userId)=>{
+  const courseDetailHandler=(userId)=>{
     console.log(userId);
   }
 
@@ -26,27 +28,29 @@ function AdminCourses() {
     // <div>Users</div>
     <>
     <Grid minH={'100vh'} templateColumns={['1fr','5fr 1fr']}>
-        <Box p={['0','16']} overflowX={'auto'}>
+        <Box p={['0','8']} overflowX={'auto'}>
           {/* <Heading */}
           <Heading textTransform={'uppercase'} children='All Users' my='16' textAlign={['center','left']} />
 
           <TableContainer w={['100vw','full']}>
             <Table variant={'simple'} size={'lg'}>
-              <TableCaption>All available users in the database</TableCaption>
+              <TableCaption>All available courses in the database</TableCaption>
               <Thead>
                 <Tr>
                   <Th>Id</Th>
-                  <Th>Name</Th>
-                  <Th>Email</Th>
-                  <Th>Role</Th>
-                  <Th>Subscription</Th>
+                  <Th>Poster</Th>
+                  <Th>Title</Th>
+                  <Th>Category</Th>
+                  <Th>Creator</Th>
+                  <Th isNumeric>Views</Th>
+                  <Th isNumeric>Lectures</Th>
                   <Th isNumeric>Action</Th>
                 </Tr>
               </Thead>
               <Tbody>
                 {
-                  users.map(item=>(
-                    <Row updateHandler={updateHandler} deleteButtonHandler={deleteButtonHandler} key={item._id} item={item} />
+                  courses.map(item=>(
+                    <Row courseDetailHandler={courseDetailHandler} deleteButtonHandler={deleteButtonHandler} key={item._id} item={item} />
                   ))
                 }
               </Tbody>
@@ -62,17 +66,21 @@ function AdminCourses() {
 export default AdminCourses
 
 
-function Row({item,updateHandler,deleteButtonHandler}){
+function Row({item,courseDetailHandler,deleteButtonHandler}){
   return (
     <Tr>
       <Td>#{item._id}</Td>
-      <Td>{item.name}</Td>
-      <Td>{item.email}</Td>
-      <Td>{item.role}</Td>
-      <Td>{item.subscription.status==='active'?'Active':'Not'}</Td>
+      <Td>
+        <Image src={item.poster.url} />
+      </Td>
+      <Td>{item.title}</Td>
+      <Td textTransform={'uppercase'}>{item.category}</Td>
+      <Td>{item.createdBy}</Td>
+      <Td isNumeric>{item.views}</Td>
+      <Td isNumeric>{item.numOfVideos}</Td>
       <Td isNumeric>
         <HStack justifyContent={'flex-end'}>
-          <Button onClick={()=>updateHandler(item._id)} variant={'outline'} color='purple.500'>Change Role</Button>
+          <Button onClick={()=>courseDetailHandler(item._id)} variant={'outline'} color='purple.500'>View Lecture</Button>
           <Button onClick={()=>deleteButtonHandler(item._id)} color={'purple.600'}>
             <RiDeleteBin7Fill />
           </Button>
